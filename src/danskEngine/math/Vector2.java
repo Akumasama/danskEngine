00caa61831b779589ad6ada290959768359472f5
Randy Schouten <hydro_gamer@hotmail.com>
2015-01-14 19:56:29 +0100

    Added function info
    Made Vector2.Zero() static
    Added Vector2.One()
    Added Vector2.toString()

--- src/danskEngine/math/Vector2.java
+++ src/danskEngine/math/Vector2.java
@@ -5,15 +5,21 @@ public class Vector2 {
     public float x;
     public float y;
 
+    /**
+     * Construct a new {@link Vector2}
+     * @param x Length on X axis
+     * @param y Length on Y axis
+     */
     public Vector2(float x, float y){
         this.x = x;
         this.y = y;
     }
     
-    public Vector2 Zero(){
-    	return new Vector2(0,0);
-    }
-    
+    /**
+     * Retrieve this {@link Vector2}'s length
+     *
+     * @return Returns this {@link Vector2}'s length
+     */
     public float length(){
     	float tempx = x;
     	float tempy = y;
@@ -24,6 +30,11 @@ public class Vector2 {
     	return (float) Math.sqrt(Math.pow(tempx, 2) + Math.pow(tempy, 2));
     }
     
+    /**
+     * Normalizes the vector to length 1
+     *
+     * @return Returns this {@link Vector2} where length is 1
+     */
     public Vector2 Normalize(){
     	float l = length();
     	x /= l;
@@ -31,12 +42,83 @@ public class Vector2 {
     	return this;
     }
     
-    //STATIC STUFF
+    /**
+     * Adds the specified {@link Vector2} to this
+     *
+     * @param a Vector2 a
+     * @return {@link Vector2} where Vector2(x1 + x2, y1 + y2)
+     */
+    public Vector2 add(Vector2 a){
+    	x += a.x;
+    	y += a.y;
+    	return this;
+    }
+    
+    /**
+     * Substracs the specified {@link Vector2} from this
+     *
+     * @param a Vector2 a
+     * @return {@link Vector2} where Vector2(x1 - x2, y1 - y2)
+     */
+    public Vector2 substract(Vector2 a){
+    	x -= a.x;
+    	y -= a.y;
+    	return this;
+    }
+    
+    /*
+     * Static functions
+     */
+    /**
+     * Adds two {@link Vector2}
+     *
+     * @param a Vector2 a
+     * @param b Vector2 b
+     * @return {@link Vector2} where Vector2(x1 + x2, y1 + y2)
+     */
     public static Vector2 add(Vector2 a, Vector2 b){
     	return new Vector2(a.x + b.x, a.y + b.y);
     }
     
+    /**
+     * Substracs two {@link Vector2}
+     *
+     * @param a Vector2 a
+     * @param b Vector2 b
+     * @return {@link Vector2} where Vector2(x1 - x2, y1 - y2)
+     */
     public static Vector2 subtract(Vector2 a, Vector2 b){
     	return new Vector2(a.x - b.x, a.y - b.y);
+    }
+    
+    /**
+     * Get a new {@link Vector2} with length 0
+     *
+     * @return Returns a new {@link Vector2} where length is 0
+     */
+    public static Vector2 Zero(){
+    	return new Vector2(0,0);
+    }
+    
+    /**
+     * Get a new {@link Vector2} with length 1
+     *
+     * @return Returns a new {@link Vector2} where length is 1
+     */
+    public static Vector2 One(){
+    	return new Vector2(1,0);
+    }
+    
+    /**
+     * Returns the Vector2 as a string
+     */
+    @Override
+    public String toString(){
+    	StringBuilder string = new StringBuilder(); //String builder is faster than string + string
+    	string.append("X: ");
+    	string.append(x);
+    	string.append(" Y: ");
+    	string.append(y);
+    	return string.toString();
     }
 }
