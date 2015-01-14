40996aec31451d8d13285155f22c817c101aae29
Danny de Bruijne <dannydb44@gmail.com>
2015-01-14 19:18:05 +0100

    Add Zero, length, add and subtract functions.

--- src/danskEngine/math/Vector2.java
+++ src/danskEngine/math/Vector2.java
@@ -9,4 +9,34 @@ public class Vector2 {
         this.x = x;
         this.y = y;
     }
+    
+    public Vector2 Zero(){
+    	return new Vector2(0,0);
+    }
+    
+    public float length(){
+    	float tempx = x;
+    	float tempy = y;
+    	
+    	if(tempx < 0) tempx *= -1;
+    	if(tempy < 0) tempy *= -1;
+    	
+    	return (float) Math.sqrt(Math.pow(tempx, 2) + Math.pow(tempy, 2));
+    }
+    
+    public Vector2 Normalize(){
+    	float l = length();
+    	x /= l;
+    	y /= l;
+    	return this;
+    }
+    
+    //STATIC STUFF
+    public static Vector2 add(Vector2 a, Vector2 b){
+    	return new Vector2(a.x + b.x, a.y + b.y);
+    }
+    
+    public static Vector2 subtract(Vector2 a, Vector2 b){
+    	return new Vector2(a.x - b.x, a.y - b.y);
+    }
 }
